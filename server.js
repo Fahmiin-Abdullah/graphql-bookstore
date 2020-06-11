ab36db945bcbabe8ae1f27720015494b06dc10ce
fahmiin <fahmiin@tyne-solutions.com>
2020-06-11 23:00:38 +0800

    Set up server to use graphql

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use('/graphql', graphqlHTTP());
 
 app.listen(port, () => console.log(`Server running on port ${port}...`));
