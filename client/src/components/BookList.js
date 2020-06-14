09062febf9fb6d23482dd5f32849f6614bb88347
fahmiin <fahmiin@tyne-solutions.com>
2020-06-15 01:19:14 +0800

    Added component to display single book details

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
 
+// Components
+import BookDetails from './BookDetails';
+
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
   displayBooks() {
     const data = this.props.data;
     if (data.loading) return(<div>Loading books...</div>);
     else {
       return data.books.map(book => {
-        return(<li key={ book.id }>{ book.name }</li>);
+        return(<li key={ book.id } onClick={ e => this.setState({ selected: book.id }) } >{ book.name }</li>);
       });
     }
   }
@@ -18,6 +27,7 @@ class BookList extends Component {
         <ul id="book-list">
           { this.displayBooks() }
         </ul>
+        <BookDetails bookId={ this.state.selected }/>
       </div>
     );
   }
--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -27,4 +27,23 @@ const createBookMutation = gql`
   }
 `
 
-export { getAuthorsQuery, getBooksQuery, createBookMutation };
+const getBookQuery = gql`
+  query($id: ID) {
+    book(id: $id) {
+      id
+      name
+      genre
+      author {
+        id
+        name
+        age
+        books {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+export { getAuthorsQuery, getBooksQuery, createBookMutation, getBookQuery };
